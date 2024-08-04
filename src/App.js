2d0cbbf8dc11b1dd9c9ea774b4ee249f83f026d3
Ankit-0009 <ankitcode2511@gmail.com>
2024-08-04 16:31:36 +0530

    Basic Routing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Footer from "./components/Layouts/Footer";
+import Header from "./components/Layouts/Header";
+import AllRoutes from "./routes/AllRoutes";
+
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Header/>
+      <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
