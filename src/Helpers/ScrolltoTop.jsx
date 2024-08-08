e5b7cab31bad73c10bb12a9a5f1626a036f30c22
Ankit-0009 <ankitcode2511@gmail.com>
2024-08-09 00:59:52 +0530

    Add ScrollToTop

--- src/Helpers/ScrolltoTop.jsx
+++ src/Helpers/ScrolltoTop.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
 
 const ScrolltoTop = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+  
+    return null;
 }
 
 export default ScrolltoTop
