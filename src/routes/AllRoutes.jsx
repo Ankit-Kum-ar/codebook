09ac8058fc3db75d4a6869b73534c14a769e0ccb
Ankit-0009 <ankitcode2511@gmail.com>
2024-08-09 00:59:19 +0530

    Routes Updated for HomePage && ProductList

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "../pages/Home/HomePage"
 import ProductsList from "../pages/Products/ProductsList"
+import ScrolltoTop from "../Helpers/ScrolltoTop"
 
  const AllRoutes = () => {
    return (
      <>
+        <ScrolltoTop/>
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/products" element={<ProductsList/>}/>
