ae65af52dc1b25c720079765001c7720114f2c17
Ankit-0009 <ankitcode2511@gmail.com>
2024-08-10 00:30:09 +0530

    Making different rating stars for each product using Rating.jsx

--- src/pages/Products/ProductsList.jsx
+++ src/pages/Products/ProductsList.jsx
@@ -19,7 +19,7 @@ const ProductsList = () => {
       <section>
         {/* Code for three dots to open menu */}
         <div className="flex items-center justify-between p-5 border-b border-gray-200 dark:border-gray-700">
-          <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">All ebooks (15)</h2>
+          <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">All ebooks ({products && products.length})</h2>
           <button onClick={() => setShow(!show)} className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none">
             <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M4 6h16M4 12h16m-7 6h7"></path>
