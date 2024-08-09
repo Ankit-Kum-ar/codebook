1d653d10b051bfb252e118f272fa469b50cec64f
Ankit-0009 <ankitcode2511@gmail.com>
2024-08-09 15:59:17 +0530

    Complete the faq section on the home screen

---
+++ src/utils/constant.js
@@ -0,0 +1,33 @@
+export const faqArray = [
+    {
+        id : 1,
+        question : "What is Codebook?",
+        answer : "Codebook is an online learning platform where you can purchase and access a variety of coding courses, ranging from beginner to advanced levels, to enhance your skills in software development, web development, data science, and more."
+    },
+    {
+        id : 2,
+        question : "What payment methods do you accept?",
+        answer : "We accept major credit/debit cards, PayPal, and other digital payment methods. All transactions are secure and encrypted."
+    },
+    {
+        id : 3,
+        question : "What types of courses are available on Codebook?",
+        answer : "We offer a wide range of courses in various domains including web development, mobile app development, data science, machine learning, and more. Each course is designed by industry experts to provide you with practical knowledge and hands-on experience."
+    },
+    {
+        id : 4,
+        question : "Can I access the course materials after completing the course?",
+        answer : "Yes, once you've purchased a course, you have lifetime access to all the course materials, including any updates or additional content added in the future."
+    },
+    {
+        id : 5,
+        question : "What is your refund policy?",
+        answer : "We offer a 30-day money-back guarantee for all our courses. If you're not satisfied with a course, you can request a refund within 30 days of purchase. Please note that refunds are not applicable if a significant portion of the course has been completed."
+    },
+    {
+        id : 6,
+        question : "Do I get a certificate after completing a course?",
+        answer : "Yes, upon successful completion of a course, you'll receive a digital certificate that you can share on your LinkedIn profile, resume, or social media platforms."
+    }
+
+]
