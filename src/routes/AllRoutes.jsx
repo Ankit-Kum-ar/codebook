000f73f2d149dd40f2d432f6b1deb5a9c28b56aa
Ankit-0009 <ankitcode2511@gmail.com>
2024-08-11 00:25:32 +0530

    Created Each Product Detail Page

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "../pages/Home/HomePage"
 import ProductsList from "../pages/Products/ProductsList"
 import ScrolltoTop from "../Helpers/ScrolltoTop"
+import { ProductDetail } from "../pages/ProductDetail"
 
  const AllRoutes = () => {
    return (
@@ -10,6 +11,7 @@ import ScrolltoTop from "../Helpers/ScrolltoTop"
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="/products" element={<ProductsList/>}/>
+            <Route path="/products/:id" element={<ProductDetail/>}/>
         </Routes>
      </>
    )
